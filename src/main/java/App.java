dd67db8c0b4dcc61780134b1fd3c533dadafa528
Jennifer Morkunas & Mala Karpy <student@epicodus.com>
2015-08-18 12:40:57 -0700

    code working

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,6 +2,7 @@ import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+import java.util.ArrayList;
 
 public class App {
   public static void main(String[] args) {
@@ -10,8 +11,8 @@ public class App {
 
     get("/", (request, response) -> {
         HashMap<String, Object> model = new HashMap<String, Object>();
-        model.put("place", request.session().attribute("place"));
-        
+        model.put("places", request.session().attribute("places"));
+
         model.put("template", "templates/index.vtl");
         return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
@@ -19,9 +20,20 @@ public class App {
     post("/places", (request, response) -> {
      HashMap<String, Object> model = new HashMap<String, Object>();
 
+
+     // array name is name of Class
+     ArrayList<Places> places = request.session().attribute("places");
+
+     if (places == null) {
+       places = new ArrayList<Places>();
+       request.session().attribute("places", places);
+ }
+
      String newPlace = request.queryParams("newPlace");
      Places addedPlace = new Places(newPlace);
-     request.session().attribute("place", addedPlace);
+
+     // adding the Place object as opposed to the String from user
+     places.add(addedPlace);
 
      model.put("template", "templates/places.vtl");
      return new ModelAndView(model, layout);
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -4,8 +4,8 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import static org.assertj.core.api.Assertions.assertThat;
-
-
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+import java.util.ArrayList;
 
 public class AppTest extends FluentTest {
   public WebDriver webDriver = new HtmlUnitDriver();
@@ -33,11 +33,33 @@ public class AppTest extends FluentTest {
   }
 
   @Test
+    public void placeIsDisplayedTest() {
+      goTo("http://localhost:4567/");
+      fill("#newPlace").with("Portland");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      assertThat(pageSource()).contains("Portland");
+    }
+
+  @Test
   public void multipleplacesDisplayedTest() {
     goTo("http://localhost:4567/");
     fill("#newPlace").with("Portland");
     submit(".btn");
     assertThat(pageSource()).contains("Your new place has been saved.");
+  }
+
+  @Test
+  public void multiplePlacesAreDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#newPlace").with("Portland");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    fill("#description").with("California");
+    submit(".btn");
+    click("a", withText("Go Back"));
+    assertThat(pageSource()).contains("Portland");
+    assertThat(pageSource()).contains("California");
   }
 
 }
